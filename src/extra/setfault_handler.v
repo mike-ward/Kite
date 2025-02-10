f75eab6a063cadf9ad3310556b2ffeda5a798b9b
mike-ward <mike@mike-ward.net>
2025-02-10 12:41:18 -0600

    add elapsed time to segfault handler

--- src/extra/setfault_handler.v
+++ src/extra/setfault_handler.v
@@ -4,13 +4,13 @@ import os
 import time
 
 pub fn install_kite_segmentation_fault_handler() {
-	os.signal_opt(.segv, kite_segmentation_fault_handler) or { panic(err) }
-}
-
-fn kite_segmentation_fault_handler(signal os.Signal) {
-	t := time.now()
-	signal_number := int(signal)
-	eprintln('signal ${signal_number}: segmentation fault: ${t}')
-	print_backtrace()
-	exit(128 + signal_number)
+	start := time.now()
+	handler := fn [start] (signal os.Signal) {
+		t := time.now()
+		signal_number := int(signal)
+		eprintln('signal ${signal_number}: segmentation fault: ${t} (${t - start})')
+		print_backtrace()
+		exit(128 + signal_number)
+	}
+	os.signal_opt(.segv, handler) or { panic(err) }
 }
